183de07d70ba4438075e206f1018d9f6ce19db05
Johanna <johanna.annau@gmail.com>
2021-01-15 17:21:33 +0100

    Part 1 and 2 done!

--- weather_assign.py
+++ weather_assign.py
@@ -1,15 +1,10 @@
-import json
 #Script Settings:
-seattle_measurements = []
+import json
 
-#read in the csv and find the code from there later
-#with open ('stations.csv') as file:
-
-# First we open the json file and load it into python, calling it 'rain'
+# First we open the json file and load it into python, calling it all measurments
 with open ('precipitation.json') as f:
     all_measurements= json.load(f)
 
-#with open ('seatle_rain.csv', 'w') as seatle:
 # then we only include the measurments for the station we are interested in, calling it by its code
 
 monthly_rain=[0]*12
@@ -22,10 +17,11 @@ for measurement in all_measurements:
 #And we have a list of monthly rainfall, where the position corresponds to the month!      
 print(monthly_rain)
 
-
+#And dumping it into a Json file:
 with open ('results1.json', 'w') as file:
      json.dump (monthly_rain, file, indent=4)
 
+#Part 2
 #to get the yearly total rain for Seattle, we do the same stuff again!
 monthly_rain=[0]*12
 monthly_relative= []
@@ -40,8 +36,10 @@ for measurement in all_measurements:
     #And now we find the relative rainfall
 for total_rain_month in monthly_rain:
     monthly_relative_values=((total_rain_month/yearly_total_rain)*100)
-    monthly_relative.append(monthly_relative_values)
-
+    monthly_relative.append(round(monthly_relative_values,2))
+#And we get a list with relative rainfalls, where the position corresponds to the month of 2010!
 print(monthly_relative)
+
+#And adding this list into a json file
 with open ('results2.json', 'w') as file:
      json.dump (monthly_relative, file, indent=4)
