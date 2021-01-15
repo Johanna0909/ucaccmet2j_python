f48f81ed2e1ae4131e833f4c0a7d80d7be546a99
Johanna <johanna.annau@gmail.com>
2021-01-15 16:22:14 +0100

    Part 1 almost done

--- weather_assign.py
+++ weather_assign.py
@@ -1,20 +1,39 @@
+import json
 #Script Settings:
-filtered = []
-#with open ('stations.csv') as stations:
- #   print(file)
+seattle_measurements = []
+
+#read in the csv and find the code from there later
+#with open ('stations.csv') as file:
 
 # First we open the json file and load it into python, calling it 'rain'
-import json
 with open ('precipitation.json') as f:
-    rain= json.load(f)
-with open ('seatle_rain.json', 'w') as file:
-# then we only include the measurments for the station we are intersted, calling it by its code
-    for measurment in rain:
-        if 'US1WAKG0038' in measurment['station']: 
-            filtered.append(measurment)
-            file.write(f"{measurment['date']},{measurment['value']}\n")
-print(file)
+    all_measurements= json.load(f)
 
-    #for line in rain:
-     #   monthly = rain.split('-')
-    #print(value[''])
+#with open ('seatle_rain.csv', 'w') as seatle:
+# then we only include the measurments for the station we are interested in, calling it by its code
+
+monthly_rain=[0,0,0,0,0,0,0,0,0,0,0,0]
+for measurement in all_measurements:
+    if 'US1WAKG0038' in measurement['station']: 
+        month= int(measurement['date'].split('-')[1])
+        value= measurement['value']
+        #now I have the values and months for each measurement in Seatle. Now we got to add them together into a list!
+        monthly_rain[month-1]+=value
+#And we have a list of monthly rainfall, where the position corresponds to the month!      
+print(monthly_rain)
+
+
+with open ('results1.json', 'w') as file:
+    json.dump (monthly_rain, file, indent=4)
+
+
+#to get the yearly total rain for Seattle
+# value=0
+# yearly_rain= {'month': value}
+# for measurement in all_measurements:
+#     if 'US1WAKG0038' in measurement['station']: 
+#         value= measurement['value']
+#         #now I have all values for Seatle. Now we got to add them together!
+#         yearly_rain['year']+=value
+# #here we have the 
+# print(yearly_rain)
